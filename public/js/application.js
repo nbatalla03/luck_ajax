a9a8584a4884f037c3354c416429151b620f30b4
Nelsol Batalla <nelsolbatalla@Nelsol-Batallas-MacBook-Pro.local>
2013-09-19 16:50:35 -0500

    added ajax.

--- public/js/application.js
+++ public/js/application.js
@@ -6,5 +6,13 @@ $(document).ready(function () {
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+  $('form').click(function(event){
+  	event.preventDefault();
+  	var url = $(this).attr('action')
+
+  	$.post(url,function(response){
+  		$("#die").replaceWith(response);
+  	});
+  });
 
 });
